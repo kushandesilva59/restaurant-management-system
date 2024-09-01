868e737467a793d093cfcdbd2a9918be802cc24b
kushan de silva <kushandesilva801@gmail.com>
2024-09-01 14:46:52 +0530

    init project

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+
+
+
+const app = express()
+
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path )
+    console.log(req.method)
+    next()
+})
+
+
+
+
+//connect to db 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to the database");
+    app.listen(process.env.PORT, () => {
+        console.log('App is running on port', process.env.PORT);
+        
+    });
+  
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+
+app.get('/',(req, res)=>{
+    res.json({message: "Welcome"})
+})
+
+
+
+
